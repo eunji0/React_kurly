fa95b4e8248e8fa510ac19f0ea6a60d6e2184e39
eunji <pih9134@naver.com>
2023-01-12 00:46:46 +0900

    kur 1/12

--- clonekurly/src/Page/Content.jsx
+++ clonekurly/src/Page/Content.jsx
@@ -21,7 +21,46 @@ background-size:cover;
 background-repeat: no-repeat;
 flex: none;
 `
+const Prevbtn = styled.div`
+    border: none;
+    background-color: none;
+    transform: translate(50%, -50%) rotate(180deg);
+    position: relative;
+    left: 55px !important;
+    top:50px !important;
+`
+
+const Nexbtn =styled.div`
+    border: none;
+    background-color: none;
+`
+
+const CountBox =styled.div`
+        position: absolute;
+    color: rgb(255, 255, 255);
+    background: rgba(0, 0, 0, 0.15);
+    z-index: 10;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    width: 55px;
+    height: 23px;
+    right: 109px;
+    bottom: 200px;
+    line-height: 23px;
+    font-size: 14px;
+    font-weight: 400;
+    border-radius: 12px;
+`
+
 export default function Content(){
+    //호버
+    const [isboxHovering, setIsboxHovering] = useState(0);
+    //페이지수
+    const [current, setCurrent] = useState(0);
+
     const settings = {
       dots: false,
       infinite: true,
@@ -30,23 +69,30 @@ export default function Content(){
       slidesToScroll: 1,
       prevArrow: (
         <div>
-            <button type="button">
+            {
+                isboxHovering ? <Prevbtn type="button">
                 <Nextbtn/>
-            </button>
+            </Prevbtn>
+            :<div></div>
+            }
+            
         </div>
       ),
       nextArrow: (
         <div>
-            <button type="button">
+            {
+                isboxHovering ? <Nexbtn type="button">
                 <Nextbtn/>
-            </button>
+            </Nexbtn>
+            : <div></div>
+            }
+            
         </div>
-    ),
-
+    )
     };
 
     return (
-        <TitleDiv>
+        <TitleDiv onMouseOver={() => setIsboxHovering(1)} onMouseOut={() => setIsboxHovering(0)}>
             <Slider {...settings}>
           <div>
             <Bannerimg style={{ backgroundImage: `url(https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/129f346e-53a2-4251-bad4-d69e6dfdd048.jpg)` }}/>
@@ -58,6 +104,7 @@ export default function Content(){
           <Bannerimg style={{ backgroundImage: `url(https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/9492f171-f3a2-4769-b74a-3944983419a1.jpg)` }}/>
           </div>
         </Slider>
+        <CountBox>{current} / 3</CountBox>
         </TitleDiv>
     );
 }
